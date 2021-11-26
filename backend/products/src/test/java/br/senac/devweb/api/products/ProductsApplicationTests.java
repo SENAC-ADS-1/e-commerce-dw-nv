4b2ba9ef5a69688b13a4125506aee04b4f9f940d
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2021-11-26 14:52:39 -0300

    Setup Inicial do projeto

---
+++ backend/products/src/test/java/br/senac/devweb/api/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.api.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
