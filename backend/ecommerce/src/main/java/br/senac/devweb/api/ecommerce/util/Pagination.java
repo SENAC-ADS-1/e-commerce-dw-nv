e1d0b6d3d142cbb3808b0fd38aa1494d927c18f8
Nickolas Deluca <nickolasdeluca@live.com>
2021-11-26 19:39:15 -0300

    Implementação de pagination na API do ecommerce

---
+++ backend/ecommerce/src/main/java/br/senac/devweb/api/ecommerce/util/Pagination.java
@@ -0,0 +1,19 @@
+package br.senac.devweb.api.ecommerce.util;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@Builder
+public class Pagination {
+    private Integer pageSize;
+    private Integer selectedPage;
+    private Integer pageCount;
+    private List<?> content;
+}
