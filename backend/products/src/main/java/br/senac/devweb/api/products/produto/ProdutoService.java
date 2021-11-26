b75b662a282a5d49ada11f4b4c95b3df5895d4a1
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2021-11-26 16:25:18 -0300

    Implementação do service de produto

---
+++ backend/products/src/main/java/br/senac/devweb/api/products/produto/ProdutoService.java
@@ -0,0 +1,75 @@
+package br.senac.devweb.api.products.produto;
+
+import br.senac.devweb.api.products.categoria.Categoria;
+import br.senac.devweb.api.products.exceptions.NotFoundException;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public Produto saveProduct(ProdutoRepresentation.ProdutoRep produtoRep, Categoria categoria) {
+        return this.produtoRepository.save(
+                Produto
+                        .builder()
+                        .nome(produtoRep.getNome())
+                        .descricao(produtoRep.getDescricao())
+                        .complemento(Strings.isEmpty(produtoRep.getComplemento()) ? "" : produtoRep.getComplemento())
+                        .fabricante(produtoRep.getFabricante())
+                        .fornecedor(Strings.isEmpty(produtoRep.getFornecedor()) ? "" : produtoRep.getFornecedor())
+                        .qtde(produtoRep.getQtde())
+                        .valor(produtoRep.getValor())
+                        .unidadeMedida(produtoRep.getUnidadeMedida())
+                        .categoria(categoria)
+                        .status(Produto.Status.ATIVO)
+                        .build()
+        );
+    }
+
+    public Produto getProduct(Long id) {
+        BooleanExpression filter = QProduto.produto.id.eq(id)
+                .and(QProduto.produto.status.eq(Produto.Status.ATIVO));
+
+        return this.produtoRepository.findOne(filter)
+                .orElseThrow(() -> new NotFoundException("Produto não encontrado!"));
+    }
+
+    public Page<Produto> getAllProducts(Predicate filter, Pageable pageable) {
+        return this.produtoRepository.findAll(filter, pageable);
+    }
+
+    public Produto updateProduct(Long id, ProdutoRepresentation.ProdutoRep produtoRep, Categoria categoria) {
+        Produto oldProduct = this.getProduct(id);
+
+        Produto updatedProduct = oldProduct
+                .toBuilder()
+                .nome(produtoRep.getNome())
+                .descricao(produtoRep.getDescricao())
+                .complemento(Strings.isEmpty(produtoRep.getComplemento()) ? "" : produtoRep.getComplemento())
+                .fabricante(produtoRep.getFabricante())
+                .fornecedor(Strings.isEmpty(produtoRep.getFornecedor()) ? "" : produtoRep.getFornecedor())
+                .qtde(produtoRep.getQtde())
+                .valor(produtoRep.getValor())
+                .unidadeMedida(produtoRep.getUnidadeMedida())
+                .categoria(categoria)
+                .status(Produto.Status.ATIVO)
+                .build();
+
+        return this.produtoRepository.save(updatedProduct);
+    }
+
+    public void deleteProduct(Long id) {
+        Produto produto = this.getProduct(id);
+
+        produto.setStatus(Produto.Status.INATIVO);
+
+        this.produtoRepository.save(produto);
+    }
+}
