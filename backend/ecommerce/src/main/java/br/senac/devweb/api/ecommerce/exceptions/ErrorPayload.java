5d1c7f647c4e0b1318b5aea88e00019ae90c3dac
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2021-11-26 17:31:41 -0300

    Implementação do handler de exceptions na API do e-commerce

---
+++ backend/ecommerce/src/main/java/br/senac/devweb/api/ecommerce/exceptions/ErrorPayload.java
@@ -0,0 +1,18 @@
+package br.senac.devweb.api.ecommerce.exceptions;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorPayload {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+}
