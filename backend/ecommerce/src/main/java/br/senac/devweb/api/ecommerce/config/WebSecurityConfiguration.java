837bd799a3a5e0b16dae5fc56cde644f90636762
Nickolas Deluca <nickolasdeluca@live.com>
2021-11-26 20:12:55 -0300

    Implementação do módulo de segurança do Spring

---
+++ backend/ecommerce/src/main/java/br/senac/devweb/api/ecommerce/config/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package br.senac.devweb.api.ecommerce.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final String[] AUTH_WHITELIST = {
+            "/cliente/*",
+            "/endereco/*",
+            "/contato/*",
+            "/h2-console",
+            "/h2-console/*"
+    };
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+}
+
