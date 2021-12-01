53c0c92a095abb84a01b0d8d52c57ebc0034096f
Vanessa Giannoni <38120642+VanessaGiannoni@users.noreply.github.com>
2021-11-30 21:25:17 -0300

    feature/sidebar-component (#24)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './src/app/header/header.component';
+import { SidebarComponent } from './src/app/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
